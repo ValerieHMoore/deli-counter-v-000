187544a34ba3ce1c7777a1b7f0d96224c9db8c6a
Valerie Moore <periwinkle17@outlook.com>
2018-06-10 16:13:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,7 +16,8 @@ def take_a_number(any_deli,name)
     any_deli << "#{name}"
     puts "Welcome, #{name}. You are number 1 in line."
   if any_deli.length > 0 && !any_deli.include?("#{name}")
-    any_deli << ", #{name}"
+      any_deli << "#{name}"
+      any_deli
       end
   end
 end
