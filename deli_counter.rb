e7958cff09c0ccd4d0e53488c2b08cfe02f362da
Valerie Moore <periwinkle17@outlook.com>
2018-06-10 16:07:57 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def line(any_deli)
+  if any_deli.length == 0
+    puts "The line is currently empty."
+  else
+    new_array = []
+
+    any_deli.each_with_index.map do |name, index|
+      new_array << " #{index + 1}. #{name}"
+      end
+      puts "The line is currently:" + new_array.join
+  end
+end
+
+def take_a_number(any_deli,name)
+  if any_deli.length == 0
+    any_deli << "#{name}"
+    puts "Welcome, #{name}. You are number 1 in line."
+  if any_deli.length > 0 && !any_deli.include?("#{name}")
+    any_deli.push(", #{name}")
+      puts "Welcome, #{name}. You are number #{any_deli.index + 1} in line."
+      puts any_deli
+      end
+  end
+end
